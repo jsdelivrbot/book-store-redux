7756e65e5ae61632313faffd71ebfdfe257fb3a4
fattymelt <seth.m.green@gmail.com>
2017-06-21 12:30:35 -0400

    Added initial index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a new component. This compoenet should produce some HTML
+
+const App = () => {
+  return <div>Hello World!</div>
+}
+
+// Take this component's generated HTML and put it on the page
+
+ReactDOM.render(<App />, document.querySelector('.container'));
