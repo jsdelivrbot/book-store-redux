07d0523dadf68381cb21f50af23645d2f347db72
fattymelt <seth.m.green@gmail.com>
2017-06-27 13:30:53 -0400

    cleanup

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function(){
   return [
-    {title: 'Javascript : The Good Parts'},
-    {title: 'Harry Potter'},
-    {title: 'The Dark Tower'},
-    {title: 'Elequent Ruby'},
+    {title: 'Javascript : The Good Parts', pages: 101},
+    {title: 'Harry Potter', pages: 80},
+    {title: 'The Dark Tower', pages: 303},
+    {title: 'Elequent Ruby', pages: 1},
   ]
 }
