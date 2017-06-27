1eb75c7c536c7e1ec5be9c6d53bac5f6b1348574
fattymelt <seth.m.green@gmail.com>
2017-06-27 13:20:17 -0400

    initial redux hookup

---
+++ src/containers/book_detail.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookDetail extends Component {
+  render() {
+
+    if(!this.props.book){
+      return <div>Select a book to get started.</div>
+    }
+    return (
+      <div><h3>Details for</h3>
+      <div>{this.props.book.title}</div></div>
+    );
+  }
+}
+
+function mapStateToProps(state){
+  return {
+    book : state.activeBook
+  }
+}
+
+// Promote BookList from a component to a container - it needs to know
+// about this new dispatch method, selectBook. Make it available as a prop
+export default connect(mapStateToProps)(BookDetail);
